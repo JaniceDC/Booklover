7a7d64cea89fa7f2027970c2bd6d786e6587da71
Janice <janice.dipayan.chen@gmail.com>
2022-10-31 17:07:29 +1100

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className='about'>
         <div className="container">
-            <h1>About Book Lovers Corner </h1>
+            <h1>About Book Lovers Corner</h1>
             <p>Imagine yourself relaxing in a cosy corner with your book...</p>
             <p>And... BAM! --THE END-- your page reads!</p>
             <p>Well you have come to the right place to find our next read in the world of epic fantasy adventures. It is almost with certainty that you have heard of 'Lord of the Rings'. If not, this is the perfect opportunity.</p>
